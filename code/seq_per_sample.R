542b00287c1f4af8cdaab7f99729a53b47230617
tomkoset <tomkoset@umich.edu>
2020-06-30 11:45:32 -0400

    Check number of sequences per sample

---
+++ code/seq_per_sample.R
@@ -0,0 +1,18 @@
+library(tidyverse)
+
+seq_count_data <- read_tsv("data/mothur/peg3350.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.pick.count.summary", col_names=c("sample", "nseqs")) %>% 
+  filter(!str_detect(sample, "water")) #Removes water control samples
+  
+#If we rarefy to 1000 sequences per sample:
+n_1000 <- seq_count_data %>% filter(nseqs < 1000) %>% select(sample) %>% nrow()
+# We'll lose 0 samples
+
+#If we rarefy to 1500 sequences per sample
+n_1500 <- seq_count_data %>% filter(nseqs < 1500) %>% select(sample) %>% nrow()
+# We'll lose 1 sample
+
+#If we rarefy to 2000 sequences per sample
+n_2000 <- seq_count_data %>% filter(nseqs < 2000) %>% select(sample) %>% nrow()
+# We'll lose 3 samples
+
+
