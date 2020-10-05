97c5b6aea14ed5a9d6478526f939d901bbecac8a
tomkoset <tomkoset@umich.edu>
2020-10-05 10:49:05 -0400

    Remove code that reads in mothur output files that will be used across all 3 subset analyses of the 16S data

--- code/5_days_PEG_16S.R
+++ code/5_days_PEG_16S.R
@@ -1,4 +1,5 @@
 source("code/utilities.R") #Loads libraries, reads in metadata, functions
+source("code/16S_common_files.R") #Reads in mothur output files
 
 #Define color scheme to match 5_days_PEG plots----
 color_scheme <- c("#238b45", "#88419d", "#f768a1", "#225ea8") #Adapted from http://colorbrewer2.org/#type=sequential&scheme=BuPu&n=4
@@ -7,40 +8,6 @@ color_labels <- c( "Clind.", "5-day PEG 3350", "5-day PEG 3350 + Clind.", "5-day
 
 metadata <- metadata %>%
   mutate(day = as.integer(day))  #Day variable (transformed to integer to get rid of decimals on PCoA animation
-
-#Lost 3 samples since we subsampled to 2000 sequences per sample
-pcoa_data <- read_tsv("data/process/peg3350.opti_mcc.braycurtis.0.03.lt.ave.pcoa.axes") %>%
-  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
-  rename(unique_label = group) %>% #group is the same as id in the metadata data frame
-  mutate(unique_label = replace(unique_label, unique_label == "M5WMR5D6", "M5WMR5Dn4"), #correct unique_labels that have typos so that they'll match up with metadata
-         unique_label = replace(unique_label, unique_label == "M5WMR6D6", "M5WMR6Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D6", "M5WMR7Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D6", "M5WMR8Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D6", "M5WMR9Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D6", "M5WMR10Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D11", "M5WMR5D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D11", "M5WMR6D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D11", "M5WMR7D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D11", "M5WMR8D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D11", "M5WMR9D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D11", "M5WMR10D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D14", "M5WMR5D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D14", "M5WMR6D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D14", "M5WMR7D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D14", "M5WMR8D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D14", "M5WMR9D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D14", "M5WMR10D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D20", "M5WMR5D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D20", "M5WMR6D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D20", "M5WMR7D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D20", "M5WMR8D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D20", "M5WMR9D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D20", "M5WMR10D10"),
-         unique_label = replace(unique_label, unique_label == "M5C19D1", "M4C19Dn5_true"), #Correct 2 samples that were swapped while putting samples into extraction plate, but were never updated on the miseq platemap
-         unique_label = replace(unique_label, unique_label == "M4C19Dn5", "M5C19D1"), #Correct 2 samples that were swapped while putting samples into extraction plate, but were never updated on the miseq platemap
-         unique_label = replace(unique_label, unique_label == "M4C19Dn5_true", "M4C19Dn5")) %>%
-  filter(unique_label != "M8C18D4") %>% #Remove this sample, since I discovered this mouse was pregnant during tissue collection
-  left_join(metadata, by= "unique_label") #merge metadata and PCoA data frames
 
 n_per_day_summary <- pcoa_data %>% count(day)
 
@@ -78,10 +45,6 @@ plot_pcoa <- function(df){
     theme_classic()
 }
 
-#Read in .loadings file to add percent variation represented by PCoA axis
-all_axis_labels <- read_tsv("data/process/peg3350.opti_mcc.braycurtis.0.03.lt.ave.pcoa.loadings")
-all_axis1 <- all_axis_labels %>% filter(axis == 1) %>% pull(loading) %>% round(digits = 1) #Pull value & round to 1 decimal
-all_axis2 <- all_axis_labels %>% filter(axis == 2) %>% pull(loading) %>% round(digits = 1) #Pull value & round to 1 decimal
 
 #PCoA plot that combines the 2 experiments and save the plot----
 pcoa_plot <- plot_pcoa(pcoa_data)+
@@ -128,39 +91,6 @@ pcoa_gif <- animate(pcoa_animated, duration = 10, fps = 10,
 anim_save(animation = pcoa_gif, filename = 'results/5_days_PEG_pcoa_over_time.gif')
 
 #Alpha diversity analysis----
-diversity_data <- read_tsv("data/process/peg3350.opti_mcc.groups.ave-std.summary") %>%
-  filter(method == "ave") %>%
-  select(group, sobs, shannon, invsimpson, coverage) %>%
-  rename(unique_label = group) %>% #group is the same as unique_label in the metadata data frame
-  mutate(unique_label = replace(unique_label, unique_label == "M5WMR5D6", "M5WMR5Dn4"), #correct unique_labels that have typos so that they'll match up with metadata
-         unique_label = replace(unique_label, unique_label == "M5WMR6D6", "M5WMR6Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D6", "M5WMR7Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D6", "M5WMR8Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D6", "M5WMR9Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D6", "M5WMR10Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D11", "M5WMR5D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D11", "M5WMR6D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D11", "M5WMR7D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D11", "M5WMR8D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D11", "M5WMR9D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D11", "M5WMR10D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D14", "M5WMR5D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D14", "M5WMR6D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D14", "M5WMR7D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D14", "M5WMR8D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D14", "M5WMR9D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D14", "M5WMR10D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D20", "M5WMR5D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D20", "M5WMR6D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D20", "M5WMR7D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D20", "M5WMR8D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D20", "M5WMR9D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D20", "M5WMR10D10"),
-         unique_label = replace(unique_label, unique_label == "M5C19D1", "M4C19Dn5_true"), #Correct 2 samples that were swapped while putting samples into extraction plate, but were never updated on the miseq platemap
-         unique_label = replace(unique_label, unique_label == "M4C19Dn5", "M5C19D1"), #Correct 2 samples that were swapped while putting samples into extraction plate, but were never updated on the miseq platemap
-         unique_label = replace(unique_label, unique_label == "M4C19Dn5_true", "M4C19Dn5")) %>%
-  filter(unique_label != "M8C18D4") %>% #Remove this sample, since I discovered this mouse was pregnant during tissue collection
-  inner_join(metadata, by = "unique_label") #Match only the samples we have sequence data for
 
 group_day_summary <- diversity_data %>%
   group_by(group) %>%
@@ -287,96 +217,6 @@ sobs_WMR <- diversity_data %>%
 save_plot("results/figures/richness_WMR.png", sobs_WMR) #Use save_plot instead of ggsave because it works better with cowplot
 
 #OTU analysis----
-# Import taxonomy into data frame and clean up taxonomy names
-taxonomy <- read_tsv(file="data/process/peg3350.taxonomy") %>%
-  rename_all(tolower) %>% #remove uppercase from column names
-  # Split taxonomic information into separate columns for each taxonomic level
-  mutate(taxonomy=str_replace_all(taxonomy, c("\\(\\d*\\)" = "", #drop digits with parentheses around them
-                                              ';$' = "", #removes semi-colon at end of line
-                                              'Bacteria_unclassified' = 'Unclassified',
-                                              "Clostridium_" = "Clostridium ", #Remove underscores after Clostridium
-                                              "_" = " ", #Removes all other underscores
-                                              "unclassified" = "Unclassified"))) %>%
-  # Separate taxonomic levels into separate columns according to semi-colon.
-  separate(taxonomy, into=c("kingdom", "phylum", "class", "order", "family", "genus"), sep=';')
-#Warning message: ~80 OTUs that are unknown, filled in with NAs
-
-# Import otu_data for samples
-otu_data <- read_tsv("data/process/peg3350.opti_mcc.shared", col_types=cols(Group=col_character())) %>%
-  select(-label, -numOtus) %>%
-  rename(unique_label = Group) %>% #group is the same as unique_label in the metadata data frame
-  mutate(unique_label = replace(unique_label, unique_label == "M5WMR5D6", "M5WMR5Dn4"), #correct unique_labels that have typos so that they'll match up with metadata
-         unique_label = replace(unique_label, unique_label == "M5WMR6D6", "M5WMR6Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D6", "M5WMR7Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D6", "M5WMR8Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D6", "M5WMR9Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D6", "M5WMR10Dn4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D11", "M5WMR5D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D11", "M5WMR6D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D11", "M5WMR7D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D11", "M5WMR8D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D11", "M5WMR9D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D11", "M5WMR10D1"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D14", "M5WMR5D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D14", "M5WMR6D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D14", "M5WMR7D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D14", "M5WMR8D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D14", "M5WMR9D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D14", "M5WMR10D4"),
-         unique_label = replace(unique_label, unique_label == "M5WMR5D20", "M5WMR5D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR6D20", "M5WMR6D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR7D20", "M5WMR7D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR8D20", "M5WMR8D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR9D20", "M5WMR9D10"),
-         unique_label = replace(unique_label, unique_label == "M5WMR10D20", "M5WMR10D10"),
-         unique_label = replace(unique_label, unique_label == "M5C19D1", "M4C19Dn5_true"), #Correct 2 samples that were swapped while putting samples into extraction plate, but were never updated on the miseq platemap
-         unique_label = replace(unique_label, unique_label == "M4C19Dn5", "M5C19D1"), #Correct 2 samples that were swapped while putting samples into extraction plate, but were never updated on the miseq platemap
-         unique_label = replace(unique_label, unique_label == "M4C19Dn5_true", "M4C19Dn5")) %>%
-  filter(unique_label != "M8C18D4") %>% #Remove this sample, since I discovered this mouse was pregnant during tissue collection
-  gather(-unique_label, key="otu", value="count") %>%
-  mutate(rel_abund=count/2000) #Use 2000, because this is the subsampling parameter chosen.
-
-#Merge otu_data to taxonomy data frame
-agg_taxa_data <- inner_join(otu_data, taxonomy)
-
-# Function to summarize relative abundance level for a given taxonomic level (ex. genus, family, phlyum, etc.)
-agg_taxonomic_data <- function(taxonomic_level) {
-  agg_taxa_data %>%
-    group_by(unique_label, {{ taxonomic_level }}) %>% #Embracing treats the taxonomic_level argument as a column name
-    summarize(agg_rel_abund=sum(rel_abund)) %>%
-    # Merge relative abundance data to specifci taxonomic_level data
-    inner_join(., metadata, by = "unique_label") %>%
-    ungroup()
-}
-
-# Relative abundance data at the otu level:
-agg_otu_data <- agg_taxonomic_data(otu)
-
-#Rename otus to match naming convention used previously and add a column that will work with ggtext package:
-agg_otu <- agg_otu_data %>%
-  mutate(key=otu) %>%
-  group_by(key)
-taxa_info <- read.delim('data/process/peg3350.taxonomy', header=T, sep='\t') %>%
-  select(-Size) %>%
-  mutate(key=OTU) %>%
-  select(-OTU)
-agg_otu_data <- inner_join(agg_otu, taxa_info, by="key") %>%
-  ungroup() %>%
-  mutate(key=str_to_upper(key)) %>%
-  mutate(taxa=gsub("(.*);.*","\\1",Taxonomy)) %>%
-  mutate(taxa=gsub("(.*)_.*","\\1",Taxonomy)) %>%
-  mutate(taxa=gsub("(.*);.*","\\1",Taxonomy)) %>%
-  mutate(taxa=gsub(".*;","",taxa)) %>%
-  mutate(taxa=gsub("(.*)_.*","\\1",taxa)) %>%
-  mutate(taxa=gsub('[0-9]+', '', taxa)) %>%
-  mutate(taxa=str_remove_all(taxa, "[(100)]")) %>%
-  unite(key, taxa, key, sep=" (") %>%
-  mutate(key = paste(key,")", sep="")) %>%
-  select(-otu, -Taxonomy) %>%
-  rename(otu=key) %>%
-  mutate(otu=paste0(gsub('TU0*', 'TU ', otu))) %>%
-  separate(otu, into = c("bactname", "OTUnumber"), sep = "\\ [(]", remove = FALSE) %>% #Add columns to separate bacteria name from OTU number to utilize ggtext so that only bacteria name is italicized
-  mutate(otu_name = glue("*{bactname}* ({OTUnumber}")) #Markdown notation so that only bacteria name is italicized
 
 #Figure out which days we have sequencing data for from the 3 groups:
 test <- pcoa_data %>% group_by(group) %>% count(day)
