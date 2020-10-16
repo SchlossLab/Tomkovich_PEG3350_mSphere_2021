1a4b0b31defd07d7b423b98b24b99c6d71698c61
Jacob King <kingjaco@umich.edu>
2020-10-16 12:30:30 -0400

    Plot OTUs (SOBS) overtime for 1 Day PEG subset.

--- code/1_day_PEG_16S.R
+++ code/1_day_PEG_16S.R
@@ -6,14 +6,18 @@ color_scheme <- c("#225ea8", "#238b45", "#88419d") #Adapted from http://colorbre
 color_groups <- c( "1RM1", "C", "M1")
 color_labels <- c( "1-day PEG 3350 + 1-day recovery", "Clind.", "1-day PEG 3350")
 
-#Read in 1_Day_PEG metadata
-metadata <- one_day_PEG_metadata
-
+#Read in and create 1_Day_PEG metadata
+metadata <- metadata %>%
+  filter(!sample_type %in% c("cecum", "distal_colon", "proximal_colon")) %>% #Get rid of rows corresponding to tissue samples in the metadata as these will create duplicate values for mice at timepoints where tissues were also collected
+  filter(group == "C" & exp_num %in% c("M6")| #Only use C mice from this experiments. Allocated groups to figures based on paper outline.
+           group == "1RM1" & exp_num %in% c("M6R")| #Had to differentiate experiment 6 from 6R in the metadata to create unique_mouse_id that wouldn't overlap for the M1 & 1RM1 mice that are both labeled with mouse_ids that are #s1-6
+           group == "M1" & exp_num %in% c("M6"))%>%
+  mutate(group=factor(group, levels=c("C", "1RM1", "M1")))  # Make sure group is treated as a factor
 
 # Pull in diversity for alpha diversity analysis
 diversity_data <- semi_join(diversity_data, metadata, by = c("unique_label")) #Only the samples that correspond to the 1_Day_PEG Subset
 
-#Alpha Diversity Analysis - Shannon Plot
+#Alpha Diversity Analysis - Shannon Plot 
 shannon_1_Day_PEG <- diversity_data %>%
   group_by(group, day) %>%
   mutate(median_shannon = median(shannon)) %>%
@@ -44,6 +48,8 @@ save_plot("results/figures/shannon_1_Day_PEG.png", shannon_1_Day_PEG)
 diversity_data_subset <- diversity_data %>%
   filter(day %in% c(-2, -1, 0, 1, 2, 4, 7))
 
+
+#Plot Shannon Diversity overtime
 Shannon_1_Day_PEG_Overtime <- diversity_data_subset %>%
   group_by(group, day) %>%
   mutate(median_shannon = median(shannon)) %>%
@@ -62,4 +68,29 @@ Shannon_1_Day_PEG_Overtime <- diversity_data_subset %>%
         text = element_text(size = 14), # Change font size for entire plot
         axis.ticks.x = element_blank())
   save_plot("results/figures/shannon_1_Day_PEG_Overtime.png", Shannon_1_Day_PEG_Overtime)
+  
+
+#Plot sobs for 1_day_PEG subset
+  sobs_1_Day_PEG <- diversity_data_subset %>%
+    group_by(group, day) %>%
+    mutate(median_sobs = median(sobs)) %>%
+    ggplot(x = day, y = sobs, colour =  group) +
+    geom_point(mapping = aes(x = day, y = sobs, color = group, fill = group), alpha = .2, size = 1.5, show.legend = FALSE, position = position_dodge(width = 0.6)) +
+    geom_line(mapping = aes(x = day, y = median_sobs, color = group), alpha = 0.6, size = 1) +
+    scale_colour_manual(name=NULL,
+                        values=color_scheme,
+                        breaks=color_groups,
+                        labels=color_labels) +
+    scale_x_continuous(breaks = c(-2:7),
+                       limits = c(-3, 8),
+                       minor_breaks = c(-2.5:7.5))+
+    theme_classic()+
+    theme(legend.position = c(1,.25),
+          text = element_text(size = 14), # Change font size for entire plot
+          axis.ticks.x = element_blank())
+  save_plot("results/figures/richness_1_Day_PEG.png", sobs_1_Day_PEG)
+  
+  
+  
+  
   
