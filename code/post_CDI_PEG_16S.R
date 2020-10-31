6dea72d5ab4d487792752a06d32f641f7acd0949
anabtaylor <anataylo@umich.edu>
2020-10-31 13:40:23 -0400

    Generate pcoa for post CDI PEG subset

--- code/post_CDI_PEG_16S.R
+++ code/post_CDI_PEG_16S.R
@@ -8,13 +8,35 @@ color_groups <- c("C", "CWM", "FRM", "RM")
 color_labels <- c( "Clind.", "Clind. + 1-day PEG 3350", "Clind. + 3-day recovery + 1-day PEG 3350 + FMT", "Clind. + 3-day recovery + 1-day PEG 3350")
 
 
-# Pull in diversity for alpha diversity analysis using post CDI PEG subset from defined in utilties.R
-diversity_data_subset <- semi_join(diversity_data, post_cdi_PEG_metadata, by = c("unique_label")) #Only the samples that correspond to the post CDI PEG subset
 
 #Statistical Analysis----
 set.seed(19881117) #Match seed used in mothur analysis scripts
 
+
+#Plot PCoA data----
+#Pull post_CDI_PEG subset of PCoA data
+pcoa_post_cdi_peg <- all_pcoa_data %>% 
+  inner_join(post_cdi_PEG_metadata)
+
+#PCoA plot that combines the 2 experiments and save the plot----
+pcoa_plot <- plot_pcoa(pcoa_post_cdi_peg)+
+  labs(x = paste("PCoA 1 (", all_axis1, "%)", sep = ""), #Annotations for each axis from loadings file
+       y = paste("PCokA 2 (", all_axis2,"%)", sep = ""))
+save_plot(filename = paste0("results/figures/post_CDI_PEG_pcoa.png"), pcoa_plot, base_height = 5, base_width = 4.5)
+
+pcoa_plot_time <- plot_pcoa(pcoa_post_cdi_peg)+
+  labs(x = paste("PCoA 1 (", all_axis1, "%)", sep = ""), #Annotations for each axis from loadings file
+       y = paste("PCoA 2 (", all_axis2,"%)", sep = ""))+
+  theme( legend.position = "none")+ #remove legend
+  facet_wrap(~ day)
+
+
+
 #Alpha Diversity Shannon Analysis----
+
+# Pull in diversity for alpha diversity analysis using post CDI PEG subset from defined in utilties.R
+diversity_data_subset <- semi_join(diversity_data, post_cdi_PEG_metadata, by = c("unique_label")) #Only the samples that correspond to the post CDI PEG subset
+
 shannon_post_cdi_peg <- diversity_data_subset %>%
   group_by(group, day) %>%
   mutate(median_shannon = median(shannon)) %>%
@@ -40,7 +62,7 @@ shannon_post_cdi_peg <- diversity_data_subset %>%
         text = element_text(size = 14), # Change font size for entire plot
         axis.text.x= element_blank(),#Remove x axis labels
         axis.ticks.x = element_blank())
-save_plot("results/figures/shannon_post_CDI_PEG.png", shannon_post_cdi_peg)
+save_plot("results/figures/post_CDI_PEG_shannon.png", shannon_post_cdi_peg)
 
 #Plot Shannon over time for all 30 days for post CDI PEG subset
 shannon_post_cdi_peg_overtime_full <- plot_shannon_overtime(diversity_data_subset) +
@@ -48,7 +70,7 @@ shannon_post_cdi_peg_overtime_full <- plot_shannon_overtime(diversity_data_subse
                      limits = c(-2,35),
                      minor_breaks = c(-2.5:7.5)) +
   theme(legend.position = c(1, .15))
-save_plot("results/figures/shannon_post_cdi_peg_overtime.png", shannon_post_cdi_peg_overtime_full)
+save_plot("results/figures/post_CDI_PEG_shannon_overtime.png", shannon_post_cdi_peg_overtime_full)
 
 #Plot Shannon over time for first 10 days for post CDI PEG subset
 diversity_data_subset_10d <- diversity_data_subset %>% filter(day %in% c(-1:10))
@@ -57,5 +79,6 @@ shannon_post_cdi_peg_overtime_10d <- plot_shannon_overtime(diversity_data_subset
                      limits = c(-2,17),
                      minor_breaks = c(-2.5:7.5)) +
   theme(legend.position = c(.92, .11))
-#Haven;t finalized plot as of 10/30
+#Haven't finalized plot as of 10/30
 #save_plot("results/figures/shannon_post_cdi_peg_overtime.png", shannon_post_cdi_peg_overtime_full)
+
