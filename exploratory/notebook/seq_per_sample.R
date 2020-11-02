431ad30d63717499b0ed9897215aeabde60ee8cf
tomkoset <tomkoset@umich.edu>
2020-11-02 08:12:56 -0500

    Explore OTUs in water controls, PERMANOVA for sample type, miseq run, and ext. plate

--- exploratory/notebook/seq_per_sample.R
+++ exploratory/notebook/seq_per_sample.R
@@ -1,6 +1,7 @@
 source("code/utilities.R") #Loads libraries, reads in metadata, functions
+source("code/16S_common_files.R") #Loads 16S output files for all sequenced samples
 
-#Examine the number of sequences per sample to determine subsampling parameter
+#Examine the number of sequences per sample to determine subsampling parameter----
 
 data <- read_tsv("data/mothur/peg3350.trim.contigs.good.unique.good.filter.unique.precluster.denovo.vsearch.pick.pick.pick.count.summary", col_names=c("unique_label", "nseqs")) %>% 
   left_join(metadata, by = "unique_label")
@@ -65,3 +66,118 @@ n_2000 <- data %>% filter(nseqs < 2000) %>% select(unique_label) %>% nrow()
 #Rarefy to 4000:
 n_4000 <- data %>% filter(nseqs < 4000) %>% select(unique_label) %>% nrow()
 #Lose 291 samples.
+
+# Examination of the OTUs in the water controls with > 1000 sequences----
+#Figure out the top 10 OTUs across the 3 water controls:
+top_10_water_otus <- agg_otu_data %>% 
+  filter(str_detect(unique_label, "water")) %>% 
+  mutate(sample_type = "water") %>% 
+  group_by(sample_type, otu) %>% 
+  summarise(mean_rel_abund = mean(agg_rel_abund)) %>% 
+  top_n(10, mean_rel_abund) %>% 
+  pull(otu)
+#Plot the top 10 OTUs across the 3 water controls:
+water_otus <- agg_otu_data %>% 
+  filter(str_detect(unique_label, "water")) %>% #select only the water controls
+  filter(otu %in% top_10_water_otus) %>% #Select the top 10 water OTUs
+  ggplot(aes(fill = otu, y = agg_rel_abund, x = unique_label))+
+  geom_bar(position = "fill", stat="identity")+
+  labs(title=NULL, 
+       x=NULL,
+       y="Relative abundance")+
+  coord_flip()+
+  theme_classic()+
+  ggsave("exploratory/notebook/waters_top_10_otus.pdf")
+
+#Figure out the top 10 OTUs in each of the 3 types of tissues: 
+top_10_tissues <- agg_otu_data %>% 
+  filter(!str_detect(unique_label, "water")) %>% #Remove water controls
+  filter(!str_detect(unique_label, "FMT")) %>% #Remove FMT gavage samples
+  filter(sample_type != "stool") %>% #Remove stool samples
+  group_by(sample_type, otu) %>% 
+  summarise(mean_rel_abund = mean(agg_rel_abund)) %>% 
+  top_n(10, mean_rel_abund) %>% 
+  pull(otu)
+#Plot the top 10 OTUs in each type of tissue
+sample_type_otus <- agg_otu_data %>% 
+  #Replace NAs for any of the relevant variables (primarily just for the water and FMT samples)
+  mutate(sample_type = case_when(str_detect(unique_label, "water") ~ "water", 
+                                 str_detect(unique_label, "FMT") ~ "FMT",
+                                 TRUE ~ sample_type)) %>% 
+  filter(!str_detect(unique_label, "water")) %>% #Remove water controls or leave the water controls to see how they compare to the rest of the tissues
+  filter(!str_detect(unique_label, "FMT")) %>% #Remove FMT gavage samples
+  filter(sample_type != "stool") %>% #Remove stool samples
+  filter(otu %in% top_10_tissues) %>% #Select the top 10 water OTUs
+  ggplot(aes(fill = otu, y = agg_rel_abund, x = sample_type))+
+  geom_bar(position = "fill", stat="identity")+
+  labs(title=NULL, 
+       x=NULL,
+       y="Relative abundance")+
+  coord_flip()+
+  theme_classic()+
+  ggsave("exploratory/notebook/tissues_top_10_otus.pdf")
+  
+# See how plate number and library number contribute to community structure----
+#Statistical analysis of PCoA data (all samples)
+set.seed(19760620) #Same seed used for mothur analysis
+# Read in Bray-Curtis distance matrix that represents all sequenced samples
+all_dist <- read_dist("data/process/peg3350.opti_mcc.braycurtis.0.03.lt.ave.dist")
+
+#Get factor levels for unique_mouse_id variable:
+mouse_id_levels <- unique(as.factor(metadata$unique_mouse_id))
+#Get factor levels for unique_cage_no variable:
+cage_levels <- unique(as.factor(metadata$unique_cage_no))
+#Get factor levels for group variable:
+group_levels <- unique(as.factor(metadata$group))
+#Get factor levels for exp_num variable:
+exp_levels <- unique(as.factor(metadata$exp_num))
+#Get factor levels for day variable:
+day_levels <- unique(as.factor(metadata$day))
+#Get factor levels for sample_type variable
+sample_levels <- unique(as.factor(metadata$sample_type))
+
+#Select relevant variables from seq_prep_metadata (the plate and MiSeq library numbers the sample was sequenced in)
+prep_variables <- seq_prep_metadata %>% 
+  select(unique_label, ext_plate, miseq_run) 
+  
+#Remove the 2nd sequencing entry for the few samples that were sequenced twice 
+prep_remove <- seq_prep_metadata %>% 
+  filter(unique_label == "M3WM1Dn5" & miseq_run == "CDIp52_Motility11-13"|unique_label == "M3WM3D1" & miseq_run == "CDIp52_Motility11-13"|unique_label == "M5WMR5D10" & miseq_run == "CDIp52_Motility11-13"|unique_label == "M5WMR7D10" & miseq_run == "CDIp52_Motility11-13")
+
+#Remove the 4 samples with duplicate entries:
+prep_variables <- anti_join(prep_variables, prep_remove)
+
+#Extract sample ids from distance matrix and join to metadata and prep_variables in order to test the impact of the relevant variables on community structure
+all_variables <- tibble(unique_label = attr(all_dist, "Labels")) %>% 
+  left_join(metadata, by = "unique_label") %>% 
+  left_join(prep_variables, by = "unique_label") %>% 
+  #Replace NAs for any of the relevant variables (primarily just for the water and FMT samples)
+  mutate(sample_type = case_when(str_detect(unique_label, "water") ~ "water",
+                                 str_detect(unique_label, "FMT") ~ "FMT",
+                                 TRUE ~ sample_type)) %>% 
+  mutate(unique_mouse_id = replace_na(unique_mouse_id, "not_applicable")) %>% 
+  mutate(unique_cage_no = replace_na(unique_cage_no, "not_applicable")) %>% 
+  mutate(group = replace_na(group, "not_applicable")) %>% 
+  mutate(exp_num = replace_na(exp_num, "not_applicable")) %>% 
+  mutate(day = replace_na(day, "not_applicable")) %>% 
+  # Make sure variables of interest are treated as factors
+  mutate(unique_mouse_id = factor(unique_mouse_id, levels = mouse_id_levels),
+         unique_cage_no = factor(unique_cage_no, levels = cage_levels),
+         group = factor(group, levels = group_levels),
+         exp_num = factor(exp_num, levels = exp_levels),
+         day = factor(day, levels = day_levels),
+         sample_type = factor(sample_type, levels = c("stool", "cecum", "proximal_colon", "distal_colon", "FMT", "water"))) #include the additional factor levels added above
+
+#PERMANOVA (adonis) of all relevant variables
+all_adonis <- adonis(all_dist~(group/(sample_type*exp_num*unique_cage_no*ext_plate*miseq_run))*day, strata = all_variables$unique_mouse_id, data = all_variables, permutations = 10)
+#How to account for sample_type?
+#Error: vector memory exhausted (limit reached?)
+
+#Check subset of variables
+#Test for sample_type, ext_plate, and any interactions between them.
+sample_miseq_ext_plate_adonis <- adonis(all_dist~ sample_type*(miseq_run/ext_plate), data = all_variables, permutations = 1000)
+#Write PERMANOVA (adonis) results to .tsv
+tibble(effects = c("sample_type", "miseq_run", "miseq_run:ext_plate", "sample_type:miseq_run:ext_plate", "Residuals"),
+                             r_sq = sample_miseq_ext_plate_adonis$aov.tab$R2[1:5],
+                             p = sample_miseq_ext_plate_adonis$aov.tab$Pr[1:5]) %>% 
+  write_tsv("exploratory/notebook/adonis_sample_miseq_plate.tsv") 
