70becaf2ee8afb608eeaf2a1391c890f62b905aa
tomkoset <tomkoset@umich.edu>
2020-11-04 14:28:04 -0500

    Remove reading in subset_analysis.R since it is not needed for this script

--- code/1_day_PEG_16S.R
+++ code/1_day_PEG_16S.R
@@ -1,7 +1,5 @@
 source("code/utilities.R") #Loads libraries, reads in metadata, functions
 source("code/16S_common_files.R") #Reads in mothur output files
-source("code/subset_analysis.R") #Read in PCoA subsets
-
 
 #Define color scheme to match 1 Day Peg Plots
 color_scheme <- c("#225ea8", "#238b45", "#88419d") #Adapted from http://colorbrewer2.org/#type=sequential&scheme=BuPu&n=4
