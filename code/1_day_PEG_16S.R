e7eaeec31abb5abe6234c10925e6ce1837d70f77
Jacob King <kingjaco@umich.edu>
2020-11-08 14:29:38 -0500

    Fix naming scheme for richness/shannon plots and do statiscal analysis on OTUs

--- code/1_day_PEG_16S.R
+++ code/1_day_PEG_16S.R
@@ -8,47 +8,23 @@ color_labels <- c( "1-day PEG 3350 + 1-day recovery", "Clind.", "1-day PEG 3350"
 
 # Subset alpha diversity data (16S_common_files) to analyze one day PEG subset mice
 diversity_data_subset <- one_day_PEG_subset(diversity_data)
+div_data_pretreatment <- diversity_data_subset %>% filter(day == -2 | day == -1)  
 
 #Statistical Analysis----
 set.seed(19760620) #Same seed used for mothur analysis
 
 #Alpha Diversity Analysis - Shannon Plot 
-shannon_1_Day_PEG <- one_day %>%
-  group_by(group, day) %>%
-  mutate(median_shannon = median(shannon)) %>%
-  ggplot(aes(x=group, y=shannon, colour=group))+
-  scale_colour_manual(name=NULL,
-                      values=color_scheme,
-                      breaks=color_groups,
-                      labels=color_labels)+
-  #  scale_shape_manual(name=NULL,
-  #                     values=shape_scheme,
-  #                     breaks=shape_experiment,
-  #                     labels=shape_experiment) +
-  scale_x_discrete(guide = guide_axis(n.dodge = 2))+
-  geom_errorbar(aes(ymax = median_shannon, ymin = median_shannon), color = "gray50", size = 1)+ #Add lines to indicate the median for each group to the plot
-  geom_jitter(size=2, alpha=0.6, show.legend = TRUE) +
-  labs(title=NULL,
-       x=NULL,
-       y="Shannon Diversity Index")+
-  ylim(0, 3.5)+
-  facet_wrap(~ day)+
-  theme_classic()+
-  theme(legend.position = c(.75,.25),
-        text = element_text(size = 14), # Change font size for entire plot
-        axis.text.x= element_blank(),#Remove x axis labels
-        axis.ticks.x = element_blank())
-save_plot("results/figures/shannon_1_Day_PEG.png", shannon_1_Day_PEG)
-
-diversity_data_subset <- diversity_data %>%
+diversity_data_subset <- diversity_data_subset %>%
   filter(day %in% c(-2, -1, 0, 1, 2, 4, 7))
 
 #Plot Shannon Diversity overtime
 Shannon_1_Day_PEG_Overtime <- plot_shannon_overtime(diversity_data_subset) +
   scale_x_continuous(breaks = c(-2:7),
                      limits = c(-3,8),
-                     minor_breaks = c(-2.5:7.5))
-  save_plot("results/figures/shannon_1_Day_PEG_Overtime.png", Shannon_1_Day_PEG_Overtime)
+                     minor_breaks = c(-2.5:7.5)) +
+  #Add rectangle to signify pre-treatment 
+  geom_rect(mapping = aes(xmin = -2.25, xmax = -0.6, ymin = 2.5, ymax = Inf), fill = 'grey94', alpha = .01, color = 'red', show.legend = FALSE)
+  save_plot("results/figures/1_Day_PEG_Overtime_shannon.png", Shannon_1_Day_PEG_Overtime)
   
 
 #Plot sobs for 1_day_PEG subset
@@ -68,8 +44,10 @@ sobs_1_Day_PEG <- diversity_data_subset %>%
   theme_classic()+
   theme(legend.position = c(1,.25),
         text = element_text(size = 14), # Change font size for entire plot
-        axis.ticks.x = element_blank())
-save_plot("results/figures/richness_1_Day_PEG.png", sobs_1_Day_PEG)
+        axis.ticks.x = element_blank()) +
+  #Add rectange to signigy the pre-treatment
+  geom_rect(mapping = aes(xmin = -2.25, xmax = -0.6, ymin = 40, ymax = Inf), fill = 'grey94', alpha = .01, color = 'red', show.legend = FALSE)
+save_plot("results/figures/1_Day_PEG_richness.png", sobs_1_Day_PEG)
 
  #Pull 1_Day_PEG subset of PCoA data
 pcoa_1_day_PEG <- read_tsv("data/process/1_day_PEG/peg3350.opti_mcc.braycurtis.0.03.lt.ave.pcoa.axes") %>%
@@ -104,5 +82,39 @@ pcoa_gif <- animate(pcoa_animated, duration = 7, fps = 10,
 # Save as gif file
 anim_save(animation = pcoa_gif, filename = 'results/1_Day_PEG_pcoa_over_time.gif')
 
-  
+
+#Get exp days sequenced in both pretreatment and other days
+div_data_posttreatment <- diversity_data_subset %>% filter(day >= 0)
+
+exp_days_pretreat <- unique(div_data_pretreatment %>% pull(day))
+exp_days_seq <- unique(div_data_posttreatment %>% pull(day))
+
+
+#Function to test at the otu level:
+kruskal_wallis_otu <- function(timepoint){
+  otu_stats <- agg_otu_data %>% 
+    filter(day == timepoint) %>%
+    select(group, otu, agg_rel_abund) %>% 
+    group_by(otu) %>% 
+    nest() %>% 
+    mutate(model=map(data, ~kruskal.test(x=.x$agg_rel_abund, g=as.factor(.x$group)) %>% tidy())) %>% 
+    mutate(median = map(data, get_rel_abund_median_group)) %>% 
+    unnest(c(model, median)) %>% 
+    ungroup() 
+  #Adjust p-values for testing multiple OTUs
+  otu_stats_adjust <- otu_stats %>% 
+    select(otu, statistic, p.value, parameter, method, "C", "1RM1", "M1") %>% 
+    mutate(p.value.adj=p.adjust(p.value, method="BH")) %>% 
+    arrange(p.value.adj) %>% 
+    write_tsv(path = paste0("data/process/otu_stats_day_", timepoint, ".tsv"))
+}
+
+# Perform kruskal wallis tests at the otu level for all days of the experiment that were sequenced----
+for (d in exp_days_seq){
+  kruskal_wallis_otu(d)
+  #Make a list of significant otus across sources of mice for a specific day  
+  stats <- read_tsv(file = paste0("data/process/otu_stats_day_", d, ".tsv"))
+  name <- paste("sig_otu_day", d, sep = "") 
+  assign(name, pull_significant_taxa(stats, otu))
+}
   
