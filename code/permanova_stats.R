859ce8825188411b1868d44ad2ce2ada95da7278
tomkoset <tomkoset@umich.edu>
2020-12-04 12:34:24 -0500

    Prepare to run PERMANOVA on Great Lakes

--- code/permanova_stats.R
+++ code/permanova_stats.R
@@ -8,9 +8,18 @@ rm(agg_otu_data, diversity_data)
 
 #PERMANOVA of 5-days PEG subset----
 #Stools
-five_d_stool <- read_dist("data/process/5_day_PEG/stools/peg3350.opti_mcc.braycurtis.0.03.lt.ave.dist")
+five_d_stools <- read_dist("data/process/5_day_PEG/stools/peg3350.opti_mcc.braycurtis.0.03.lt.ave.dist")
+five_d_stools_variables <- tibble(unique_label = attr(five_d_stools, "Labels")) %>%
+  left_join(metadata, by = "unique_label")
+five_d_stools_adonis <- adonis(five_d_stools~(group/(sample_type*exp_num*unique_cage_no*ext_plate*miseq_run))*day, strata = five_d_stools_variables$unique_mouse_id, data = five_d_stools_variables, permutations = 10)
+five_d_stools_adonis 
 #Tissues
 five_d_tissues <- read_dist("data/process/5_day_PEG/tissues/peg3350.opti_mcc.braycurtis.0.03.lt.ave.dist")
+five_d_tissues_variables <- tibble(unique_label = attr(five_d_tissues, "Labels")) %>%
+  left_join(metadata, by = "unique_label")
+#PERMANOVA of all relevant variables (exclude Miseq run since they were the same for all samples in this subset)
+five_d_tissues_adonis <- adonis(five_d_tissues~(group/(sample_type*exp_num*unique_cage_no*ext_plate))*day, strata = five_d_tissues_variables$unique_mouse_id, data = five_d_tissues_variables, permutations = 10)
+five_d_tissues_adonis
 
 #PERMANOVA of 1-day PEG subset----
 
---
+++ code/slurm/permanova.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+###############################
+#                             #
+#  1) Job Submission Options  #
+#                             #
+###############################
+
+# Name
+#SBATCH --job-name=permanova_test
+
+# Resources
+# For MPI, increase ntasks-per-node
+# For multithreading, increase cpus-per-task
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=50GB
+#SBATCH --time=24:00:00
+
+# Account
+#SBATCH --account=pschloss1
+#SBATCH --partition=standard
+
+# Logs
+#SBATCH --mail-user=tomkoset@umich.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --output=%x-%j.out
+
+# Environment
+#SBATCH --export=ALL
+
+# To run r & vegan adonis(): conda activate rstats before submitting job.
+# --max-ppsize 500000 suggested by Kelly to work around memory issues
+
+#####################
+#                   #
+#  2) Job Commands  #
+#                   #
+#####################
+
+Rscript --max-ppsize 500000 code/permanova_stats.R
