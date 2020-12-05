6a9ea879bfd35ac16159d06d7a5adc096b5802a2
tomkoset <tomkoset@umich.edu>
2020-12-05 15:45:19 -0500

    Community type analysis across groups at baseline, treatment, post-treatment, and post-infection grouped timepoints

--- code/community_type_analysis.R
+++ code/community_type_analysis.R
@@ -207,7 +207,10 @@ percent_day <- sample_best_community_fit %>%
   theme(axis.title.y = element_blank(), #Get rid of y axis title
         axis.title.x = element_blank(), #Get rid of x axis title, text, and ticks. Will combine with bacteria in community
         axis.text.x = element_blank(),
-        axis.ticks.x = element_blank())  
+        axis.ticks.x = element_blank())
+
+#3 waters all belong to community type 13
+# 3 FMTs belong to community types 1, 8, and 11
 
 #Combine plots of percent of each group or sample type that belong to each cluster with the
 #plot of the bacteria relative abundances within each cluster
@@ -222,4 +225,126 @@ plot_grid(combined_heatmaps_group[[1]], combined_heatmaps_group[[2]], rel_height
   ggsave("results/figures/community_types_group.png", height = 8.0, width = 8.5)
 plot_grid(combined_heatmaps_day[[1]], combined_heatmaps_day[[2]], rel_heights = c(1.5, 1), ncol = 1)+
   ggsave("results/figures/community_types_day.png", height = 10.5, width = 8.5)
+
+#Community types across groups, facetted by experimental milestones: Baseline, Post-treatment, Post-treatment-----
+#First transform timepoints to milestones
+group_time_fit <- sample_best_community_fit %>% 
+  filter(!group %in% c("FMT", "water")) %>% #Remove 3 water & 3 FMT samples for this analysis
+  #Transform day variable into an integer variable to help with creating timepoint milestones
+  mutate(timepoint_status = case_when(group == "WMR" & day == -15 ~ "baseline",
+                                      group == "WMR" & day %in% c(-10, -5, -4, -2, -1, 0) ~ "post-treatment",
+                                      group %in% c("C", "CWM", "RM", "FRM") & day < 0 ~"baseline",
+                                      group %in% c("C", "CWM", "RM", "FRM") & day == 0 ~"post-treatment",
+                                      group %in% c("WM", "WMC") & day == -5 ~"baseline",
+                                      group %in% c("WM", "WMC") & day == -1 ~ "treatment",
+                                      group %in% c("WM", "WMC") & day == 0 ~ "post-treatment",
+                                      group == "1RM1" & day == -2 ~ "baseline",
+                                      group == "1RM1" & day == 0 ~ "post-treatment",
+                                      group == "M1" & day %in% c(-1, -11) ~ "baseline",
+                                      group == "M1" & day ==0 ~ "post-treatment",
+                                      group %in% c("WMN", "CN") & day == -5 ~"baseline",
+                                      group == "CN" & day == -1 ~"baseline",
+                                      group == "WMN" & day == -1 ~ "treatment",
+                                      group == "WMN" & day %in% c(0, 4, 6, 30) ~ "post-treatment",
+                                      group == "CN" & day %in% c(0, 4) ~"post-treatment",
+                                 TRUE ~ "post-infection")) %>% 
+  select(exp_num, group, day, timepoint_status, community)
+
+#Make heatmap of group communitty fit, faceted by timepoint status
+#Make labels for facets
+facet_labels <- c("Baseline", "Treatment", "Post-treatment", "Post-infection")
+names(facet_labels) <- c("baseline", "treatment", "post-treatment", "post-infection")
+group_time_plot <- group_time_fit %>% 
+  mutate(timepoint_status = fct_relevel(timepoint_status, "baseline", "treatment", "post-treatment", "post-infection")) %>%   
+  group_by(community, group, timepoint_status) %>% 
+  summarize(group_community_total = n()) %>%
+  #Make a new variable % group per community type based on total community numbers
+  mutate(percent_community = case_when(community == "1" ~ (group_community_total/74)*100,
+                                       community == "2" ~ (group_community_total/80)*100,
+                                       community == "3" ~ (group_community_total/63)*100,
+                                       community == "4" ~ (group_community_total/112)*100,
+                                       community == "5" ~ (group_community_total/48)*100,
+                                       community == "6" ~ (group_community_total/37)*100,
+                                       community == "7" ~ (group_community_total/189)*100,
+                                       community == "8" ~ (group_community_total/120)*100,
+                                       community == "9" ~ (group_community_total/167)*100,
+                                       community == "10" ~ (group_community_total/42)*100,
+                                       community == "11" ~ (group_community_total/82)*100,
+                                       community == "12" ~ (group_community_total/100)*100,
+                                       community == "13" ~ (group_community_total/33)*100,
+                                       community == "14" ~ (group_community_total/143)*100,
+                                       community == "15" ~ (group_community_total/94)*100,
+                                       TRUE ~ 0)) %>% #No samples should fall into this category
+  mutate(group = fct_relevel(group, "CN", "C", "FRM", "RM", "CWM", "1RM1", "M1", "WMR", "WMC", "WMN", "WM")) %>% #Specify the order of the groups
+  ggplot()+
+  geom_tile(aes(x=community, y=group, fill=percent_community))+
+  facet_wrap(~timepoint_status, labeller = labeller(timepoint_status = facet_labels), nrow = 1)+
+  scale_x_continuous(breaks = c(1:15))+
+  scale_y_discrete(label = c("Clind. without infection", "Clind.", "Clind. + 3-day recovery + 1-day PEG 3350 + FMT", "Clind. + 3-day recovery + 1-day PEG 3350",
+                             "Clind. + 1-day PEG 3350", "1-day PEG 3350 + 1-day recovery", "1-day PEG 3350", "5-day PEG 3350 + 10-day recovery", "5-day PEG 3350 + Clind.", 
+                             "5-day PEG 3350 without infection", "5-day PEG 3350"))+ #Descriptive group names that match the rest of the plots
+  theme_classic()+
+  scale_fill_distiller(palette = "YlGnBu", direction = 1, name = "% Community")+
+  theme(axis.title.y = element_blank(), #Get rid of y axis title
+        plot.background=element_blank(),
+        #remove plot border
+        panel.border=element_blank(),
+        #set thickness of axis ticks
+        axis.ticks=element_line(size=0.4),
+        strip.background = element_blank(), #get rid of box around facet_wrap labels
+        axis.title.x = element_blank()) #Get rid of x axis title, text, and ticks. Will combine with bacteria in community
+save_plot(filename = "results/figures/community_types_group_timepoint_status.png", group_time_plot, base_height = 5, base_width = 16)
+      
+#Plot just post-infection timepoint with bacteria in each community
+post_infection_plot <- group_time_fit %>% 
+  mutate(timepoint_status = fct_relevel(timepoint_status, "baseline", "treatment", "post-treatment", "post-infection")) %>%   
+  filter(timepoint_status == "post-infection") %>% 
+  group_by(community, group, timepoint_status) %>% 
+  summarize(group_community_total = n()) %>%
+  #Make a new variable % group per community type based on total community numbers
+  mutate(percent_community = case_when(community == "1" ~ (group_community_total/74)*100,
+                                       community == "2" ~ (group_community_total/80)*100,
+                                       community == "3" ~ (group_community_total/63)*100,
+                                       community == "4" ~ (group_community_total/112)*100,
+                                       community == "5" ~ (group_community_total/48)*100,
+                                       community == "6" ~ (group_community_total/37)*100,
+                                       community == "7" ~ (group_community_total/189)*100,
+                                       community == "8" ~ (group_community_total/120)*100,
+                                       community == "9" ~ (group_community_total/167)*100,
+                                       community == "10" ~ (group_community_total/42)*100,
+                                       community == "11" ~ (group_community_total/82)*100,
+                                       community == "12" ~ (group_community_total/100)*100,
+                                       community == "13" ~ (group_community_total/33)*100,
+                                       community == "14" ~ (group_community_total/143)*100,
+                                       community == "15" ~ (group_community_total/94)*100,
+                                       TRUE ~ 0)) %>% #No samples should fall into this category
+  mutate(group = fct_relevel(group, "CN", "C", "FRM", "RM", "CWM", "1RM1", "M1", "WMR", "WMC", "WMN", "WM")) %>% #Specify the order of the groups
+  ggplot()+
+  geom_tile(aes(x=community, y=group, fill=percent_community))+
+  facet_wrap(~timepoint_status, labeller = labeller(timepoint_status = facet_labels), nrow = 1)+
+  scale_x_continuous(breaks = c(1:15))+
+  scale_y_discrete(label = c("Clind. without infection", "Clind.", "Clind. + 3-day recovery + 1-day PEG 3350 + FMT", "Clind. + 3-day recovery + 1-day PEG 3350",
+                             "Clind. + 1-day PEG 3350", "1-day PEG 3350 + 1-day recovery", "1-day PEG 3350", "5-day PEG 3350 + 10-day recovery", "5-day PEG 3350 + Clind.", 
+                             "5-day PEG 3350 without infection", "5-day PEG 3350"))+ #Descriptive group names that match the rest of the plots
+  theme_classic()+
+  scale_fill_distiller(palette = "YlGnBu", direction = 1, name = "% Community")+
+  theme(axis.title.y = element_blank(), #Get rid of y axis title
+        plot.background=element_blank(),
+        #remove plot border
+        panel.border=element_blank(),
+        #set thickness of axis ticks
+        axis.ticks=element_line(size=0.4),
+        strip.background = element_blank(), #get rid of box around facet_wrap labels
+        axis.title.x = element_blank(), #Get rid of x axis title, text, and ticks. Will combine with bacteria in community
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
+#Combine post-infection with bacteria in community types
+#Combine plots of percent of each group or sample type that belong to each cluster with the
+#plot of the bacteria relative abundances within each cluster
+#Align heat maps
+combined_heatmaps_group_pi <- align_plots(post_infection_plot, bacteria_in_community, align = 'v', axis = "b")
+#Plot and save the aligned heat maps
+plot_grid(combined_heatmaps_group_pi[[1]], combined_heatmaps_group_pi[[2]], rel_heights = c(1, 1.2), ncol = 1)+
+  ggsave("results/figures/community_types_group_postinfection.png", height = 8.0, width = 8.5)
+
 
