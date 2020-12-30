b28146b574ebf8e9802eb09bc175806fe98585ff
tomkoset <tomkoset@umich.edu>
2020-12-30 15:29:46 -0500

    Try decreasing number for parallel argument in permanova

--- code/5_days_PEG_permanova.R
+++ code/5_days_PEG_permanova.R
@@ -15,7 +15,7 @@ five_d_tissues <- read_dist("data/process/5_day_PEG/tissues/peg3350.opti_mcc.bra
 five_d_tissues_variables <- tibble(unique_label = attr(five_d_tissues, "Labels")) %>%
   left_join(metadata, by = "unique_label")
 #PERMANOVA of all relevant variables (exclude Miseq run since they were the same for all samples in this subset)
-five_d_tissues_adonis <- adonis(five_d_tissues~(group/(sample_type*exp_num*unique_cage_no*ext_plate))*day, strata = five_d_tissues_variables$unique_mouse_id, data = five_d_tissues_variables, permutations = 1000, parallel = 32)
+five_d_tissues_adonis <- adonis(five_d_tissues~(group/(sample_type*exp_num*unique_cage_no*ext_plate))*day, strata = five_d_tissues_variables$unique_mouse_id, data = five_d_tissues_variables, permutations = 1000, parallel = 10)
 five_d_tissues_adonis
 #Write PERMANOVA results to tsv
 five_d_tissues_adonis_table <- as_tibble(rownames_to_column(five_d_tissues_adonis$aov.tab, var = "effects")) %>% 
@@ -25,7 +25,7 @@ five_d_tissues_adonis_table <- as_tibble(rownames_to_column(five_d_tissues_adoni
 five_d_stools <- read_dist("data/process/5_day_PEG/stools/peg3350.opti_mcc.braycurtis.0.03.lt.ave.dist")
 five_d_stools_variables <- tibble(unique_label = attr(five_d_stools, "Labels")) %>%
   left_join(metadata, by = "unique_label")
-five_d_stools_adonis <- adonis(five_d_stools~(group/(exp_num*unique_cage_no*ext_plate*miseq_run))*day, strata = five_d_stools_variables$unique_mouse_id, data = five_d_stools_variables, permutations = 1000, parallel = 32)
+five_d_stools_adonis <- adonis(five_d_stools~(group/(exp_num*unique_cage_no*ext_plate*miseq_run))*day, strata = five_d_stools_variables$unique_mouse_id, data = five_d_stools_variables, permutations = 1000, parallel = 10)
 five_d_stools_adonis_table <- as_tibble(rownames_to_column(five_d_stools_adonis$aov.tab, var = "effects")) %>% 
   write_tsv("data/process/5_days_PEG_permanova_stools.tsv")
 
--- code/slurm/permanova_5d.sh
+++ code/slurm/permanova_5d.sh
@@ -14,9 +14,9 @@
 # For multithreading, increase cpus-per-task
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=34
-#SBATCH --mem=50GB
-#SBATCH --time=72:00:00
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=180GB
+#SBATCH --time=7-00:00:00
 
 # Account
 #SBATCH --account=pschloss1
