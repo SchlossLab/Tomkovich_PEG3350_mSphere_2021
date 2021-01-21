c736c86b5c38fb614efab4b4b62289547be19178
tomkoset <tomkoset@umich.edu>
2021-01-21 10:43:11 -0500

    Check how long it takes to run PERMANOVA on 5-day stool samples with 10 iterations

--- code/5_days_PEG_permanova.R
+++ code/5_days_PEG_permanova.R
@@ -6,7 +6,7 @@ set.seed(19760620) #Same seed used for mothur analysis
 #Remove variables not needed for this analysis
 rm(agg_otu_data, diversity_data)
 
-metadata <- metadata %>% 
+metadata <- metadata %>%
   mutate(day = factor(day, levels = c(unique(as.factor(day)), "-15", "-11", "-10", "-5", "-4", "-2", "-1", "0"))) #Transform day variable into factor variable
 
 #PERMANOVA of 5-days PEG subset----
@@ -18,14 +18,13 @@ five_d_tissues_variables <- tibble(unique_label = attr(five_d_tissues, "Labels")
 five_d_tissues_adonis <- adonis(five_d_tissues~(group/(sample_type*exp_num*unique_cage_no*ext_plate))*day, strata = five_d_tissues_variables$unique_mouse_id, data = five_d_tissues_variables, permutations = 1000, parallel = 10)
 five_d_tissues_adonis
 #Write PERMANOVA results to tsv
-five_d_tissues_adonis_table <- as_tibble(rownames_to_column(five_d_tissues_adonis$aov.tab, var = "effects")) %>% 
+five_d_tissues_adonis_table <- as_tibble(rownames_to_column(five_d_tissues_adonis$aov.tab, var = "effects")) %>%
   write_tsv("data/process/5_days_PEG_permanova_tissues.tsv")
 
 #Stools
 five_d_stools <- read_dist("data/process/5_day_PEG/stools/peg3350.opti_mcc.braycurtis.0.03.lt.ave.dist")
 five_d_stools_variables <- tibble(unique_label = attr(five_d_stools, "Labels")) %>%
   left_join(metadata, by = "unique_label")
-five_d_stools_adonis <- adonis(five_d_stools~(group/(exp_num*unique_cage_no*ext_plate*miseq_run))*day, strata = five_d_stools_variables$unique_mouse_id, data = five_d_stools_variables, permutations = 1000, parallel = 10)
-five_d_stools_adonis_table <- as_tibble(rownames_to_column(five_d_stools_adonis$aov.tab, var = "effects")) %>% 
+five_d_stools_adonis <- adonis(five_d_stools~(group/(exp_num*unique_cage_no*ext_plate*miseq_run))*day, strata = five_d_stools_variables$unique_mouse_id, data = five_d_stools_variables, permutations = 10, parallel = 10)
+five_d_stools_adonis_table <- as_tibble(rownames_to_column(five_d_stools_adonis$aov.tab, var = "effects")) %>%
   write_tsv("data/process/5_days_PEG_permanova_stools.tsv")
-
