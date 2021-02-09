ed81220379aa850ef4ae8940dd7508695e1cc588
JacobKing99 <kingjaco@umich.edu>
2021-02-09 11:06:24 -0500

    Figure 3 edits

--- code/figure_3.R
+++ code/figure_3.R
@@ -1,10 +1,11 @@
 source("code/utilities.R")
 
-a <- ggdraw() + draw_image("results/pictures/1_Day_PEG_Schematic.png")
+a <- ggdraw() + draw_image("results/pictures/1_Day_PEG_Schematic.png", height = .85, scale = 1.5)
 b <- ggdraw() + draw_image("results/figures/1_day_PEG_cfu.png")
-c <- ggdraw() + draw_image("results/figures/1_day_PEG_otu_peptostreptococcaceae.png")
+c <- ggdraw() + draw_image("results/figures/1_Day_PEG_PCoA.png", x = -.1)
 d <- ggdraw() + draw_image("results/figures/1_Day_PEG_shannon.png")
 left_panel <- plot_grid(a, b, labels = c("A", "B"), label_size = 12, ncol = 1)
 right_panel <- plot_grid(c, d, labels = c("C", "D"), label_size = 12, ncol = 1)
 plot_grid(left_panel, right_panel) +
   ggsave("results/figures/figure_3.pdf", width=6.875, height=4.73)
+
