62fd39df68d0d03336203e9c1284056f6d0463a3
JacobKing99 <kingjaco@umich.edu>
2021-02-15 11:13:29 -0500

    Rearrange figure 3 schematic

--- code/figure_3.R
+++ code/figure_3.R
@@ -1,8 +1,8 @@
 source("code/utilities.R")
 
-a <- ggdraw() + draw_image("results/pictures/1_Day_PEG_Schematic.png", height = .85, scale = 1.5)
+a <- ggdraw() + draw_image("results/pictures/1_Day_PEG_Schematic.png", scale = 1.5, x = .06, y = -.1)
 b <- ggdraw() + draw_image("results/figures/1_day_PEG_cfu.png")
-c <- ggdraw() + draw_image("results/figures/1_Day_PEG_PCoA.png", x = -.1)
+c <- ggdraw() + draw_image("results/figures/1_Day_PEG_PCoA.png", x = -.1, y = -.05)
 d <- ggdraw() + draw_image("results/figures/1_Day_PEG_shannon.png")
 left_panel <- plot_grid(a, b, labels = c("A", "B"), label_size = 12, ncol = 1)
 right_panel <- plot_grid(c, d, labels = c("C", "D"), label_size = 12, ncol = 1)
