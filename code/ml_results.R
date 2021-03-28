5c6d754474b22713827ea9837126212c4e8b91ff
tomkoset <tomkoset@umich.edu>
2021-03-28 15:49:32 -0400

    Update ml results and plots for genus level input data

--- code/ml_results.R
+++ code/ml_results.R
@@ -13,19 +13,19 @@ read_perf_results <- function(file_path, taxa_level_name){
   read_csv(file_path) %>% 
     mutate(taxa_level = taxa_level_name) 
 }
-#Read in OTU level model performance:
-otu_results <- read_perf_results("results/performance_results.csv", 
-                              "otu") 
+#Read in genus level model performance:
+genus_results <- read_perf_results("results/performance_results.csv", 
+                              "genus") 
 
 #Examine AUC results for each machine learning method----
-auc_results <- otu_results %>% 
+auc_results <- genus_results %>% 
   group_by(method) %>% 
   summarize(median = median(AUC)) %>% 
   arrange(desc(median))
 
-#Plot performance for all methods with OTU input data----
-performance_otu <- otu_results %>% 
-  filter(taxa_level == "otu") %>% 
+#Plot performance for all methods with genus input data----
+performance_genus <- genus_results %>% 
+  filter(taxa_level == "genus") %>% 
   mutate(method = fct_relevel(method, c("rf", "glmnet", "svmRadial"))) %>% #Reorder methods so left to right is in order of descending AUC
   ggplot(aes(x = method, y = AUC, color = method)) +
   geom_boxplot(alpha=0.5, fatten = 4) +
@@ -43,7 +43,7 @@ performance_otu <- otu_results %>%
   theme(legend.position = "none",
         text = element_text(size = 19),# Change font size for entire plot
 #        axis.ticks.x = element_blank(), #Remove x axis ticks
-#        axis.text.x = element_blank(), #Remove x axis text (all models are at the OTU level),
+#        axis.text.x = element_blank(), #Remove x axis text (all models are at the genus level),
         axis.text.x = element_text(angle = 45, hjust = 1), #Angle axis labels
         strip.background = element_blank()) +#Make Strip backgrounds blank
   guides(color=guide_legend(nrow = 2))+   #Legend in 2 rows so it doesn't get cut off 
@@ -57,28 +57,19 @@ performance_otu <- otu_results %>%
 #file_path = path to the file name
 read_feat_imp <- function(file_path){
   feat_imp <- read_csv(file_path)
-  taxa_info <- read.delim('data/process/peg3350.taxonomy', header=T, sep='\t') %>%
+  taxa_info <- read.delim('data/process/peg3350.genus.taxonomy', header=T, sep='\t') %>%
     select(-Size) %>%
     mutate(names=OTU) %>%
     select(-OTU)
   final_feat_imp <- inner_join(feat_imp, taxa_info, by="names") %>%
-    ungroup() %>%
+    ungroup() %>%    
     mutate(names=str_to_upper(names)) %>%
-    mutate(taxa=gsub("(.*);.*","\\1",Taxonomy)) %>%
-    mutate(taxa=gsub("(.*)_.*","\\1",Taxonomy)) %>%
-    mutate(taxa=gsub("(.*);.*","\\1",Taxonomy)) %>%
-    mutate(taxa=str_replace_all(taxa, c("Clostridium_" = "Clostridium "))) %>% 
-    mutate(taxa=gsub(".*;","",taxa)) %>%
-    mutate(taxa=gsub("(.*)_.*","\\1",taxa)) %>%
-    mutate(taxa=gsub('[0-9]+', '', taxa)) %>%
-    mutate(taxa=str_remove_all(taxa, "[(100)]")) %>%
-    unite(names, taxa, names, sep=" (") %>%
-    mutate(names = paste(names,")", sep="")) %>%
-    select(-Taxonomy) %>%
-    rename(otu=names) %>%
-    mutate(otu=paste0(gsub('TU0*', 'TU ', otu))) %>%
-    separate(otu, into = c("bactname", "OTUnumber"), sep = "\\ [(]", remove = FALSE) %>% #Add columns to separate bacteria name from OTU number to utilize ggtext so that only bacteria name is italicized
-    mutate(otu_name = glue("*{bactname}* ({OTUnumber}")) #Markdown notation so that only bacteria name is italicized
+    mutate(genus=gsub("(.*);.*","\\1",Taxonomy)) %>%
+    mutate(genus=gsub("(.*)_.*","\\1",Taxonomy)) %>%
+    mutate(genus=gsub("(.*);.*","\\1",Taxonomy)) %>% 
+    mutate(genus=str_replace_all(genus, c("_" = " ", #Removes all other underscores
+                                          "unclassified" = "Unclassified"))) %>% 
+    select(-Taxonomy)
   return(final_feat_imp)
 }
 
@@ -90,7 +81,7 @@ rf_feat <- read_feat_imp("results/combined_feature-importance_rf.csv") #%>%
 #df = dataframe of feature importances for the 100 seeds
 top_20 <- function(df){
   data_first_20 <- df %>% 
-    group_by(otu, bactname) %>% 
+    group_by(genus) %>% 
     summarize(median = median(perf_metric_diff)) %>% #Get the median performance metric diff. for each feature
     arrange(desc(median)) %>% #Arrange from largest median to smallest
     head(20)
@@ -98,31 +89,18 @@ top_20 <- function(df){
   return(data_first_20)
 }
 
-#Top 20 OTUs for each input dataset with the random forest model
-rf_top_feat <- top_20(rf_feat) %>% pull(otu)
+#Top 20 genera for each input dataset with the random forest model
+rf_top_feat <- top_20(rf_feat) %>% pull(genus)
 
-#Create color schemes based on OTUs with the same taxa name----
-color_scheme_df <-  top_20(rf_feat) %>% 
-  group_by(bactname) %>% 
-  mutate(duplicate = n()) %>% #count number of features with same bacteria name
-  #Create color scheme based on number of times taxa shows up in top 20
-  mutate(color = case_when(duplicate == 1 ~ "#bababa",
-                           duplicate == 2 ~ "#404040",
-                           duplicate == 3 ~ "blue",
-                           duplicate == 9 ~ "#ca0020",
-                           TRUE ~ "yellow")) #Should catch all cases with above, update if any are yellow
-color_scheme <- color_scheme_df$color
-color_bact <- color_scheme_df$bactname 
-legend_bact <- color_bact
 
 #Function to filter to top OTUs for each pairwise comparison & plot results----
 #df = dataframes of feature importances for all seeds
 #top_otus = dataframes of top otus
 #comp_name = name of comparison to title the plot (in quotes)
-plot_feat_imp <- function(df, top_otus){
+plot_feat_imp <- function(df, top_feat){
   df %>% 
-    filter(otu %in% top_otus) %>% 
-    ggplot(aes(fct_reorder(otu_name, -perf_metric_diff, .desc = TRUE), perf_metric_diff, color = bactname))+
+    filter(genus %in% top_feat) %>% 
+    ggplot(aes(fct_reorder(genus, -perf_metric_diff, .desc = TRUE), perf_metric_diff))+
     stat_summary(fun = 'median', 
                  fun.max = function(x) quantile(x, 0.75), 
                  fun.min = function(x) quantile(x, 0.25),
@@ -145,9 +123,9 @@ plot_feat_imp <- function(df, top_otus){
 
 #Plot feature importances for the top OTUs for each comparison----
 rf_feat_5dpi <- plot_feat_imp(rf_feat, rf_top_feat)+
-  ggsave("results/figures/ml_top_features_otu.png", height = 5, width = 8)
+  ggsave("results/figures/ml_top_features_genus.png", height = 5, width = 8)
 
 #Make composite figure of ML results for 5dpi----
-plot_grid(performance_otu, rf_feat_5dpi, labels = c("A", "B"), label_size = 12, ncol=1)+
-  ggsave("results/figures/ml_summary_5dpi_otu.pdf", width=5, height=8)
+plot_grid(performance_otu, rf_feat_5dpi, labels = NULL, label_size = 12, ncol=1)+
+  ggsave("results/figures/ml_summary_5dpi_genus.pdf", width=5, height=8)
   
