7d55de6c4d19fb18cf9137800d5f7928d1d5b725
tomkoset <tomkoset@umich.edu>
2021-04-20 08:08:47 -0400

    Move PCoA code for all samples combined to exploratory

--- code/16S_common_files.R
+++ code/16S_common_files.R
@@ -6,45 +6,6 @@ stool_corresponding_to_tissues <- metadata %>%
   separate(unique_label, into = c("tissue", "corresponding_stool_sample"), sep = 1) %>% 
   pull(corresponding_stool_sample)
 
-#Read in PCoA for all samples----
-all_pcoa_data <- read_tsv("data/process/peg3350.opti_mcc.braycurtis.0.03.lt.ave.pcoa.axes") %>%
-  select(group, axis1, axis2) %>% #Limit to 2 PCoA axes
-  rename(unique_label = group) %>% #group is the same as id in the metadata data frame
-  left_join(metadata, by= "unique_label")  #merge metadata and PCoA data frames
-
-
-#Read in .loadings file to add percent variation represented by PCoA axis
-all_axis_labels <- read_tsv("data/process/peg3350.opti_mcc.braycurtis.0.03.lt.ave.pcoa.loadings")
-all_axis1 <- all_axis_labels %>% filter(axis == 1) %>% pull(loading) %>% round(digits = 1) #Pull value & round to 1 decimal
-all_axis2 <- all_axis_labels %>% filter(axis == 2) %>% pull(loading) %>% round(digits = 1) #Pull value & round to 1 decimal
-
-#Plot PCoA data with the 3 waters that had > 1000 sequences 
-pcoa_water <- all_pcoa_data %>% 
-  mutate(group = ifelse(grepl("water", unique_label), "water", "sample")) %>%  #Temporarily create just 2 groups. Waters and the rest of the samples
-  ggplot(aes(x=axis1, y=axis2, color = group))+
-  geom_point(size=2)+
-  theme_classic()+
-  theme(legend.position = c(.9, .2))+
-  theme(legend.position = c(.9, .3))+
-  xlim(-0.425, 0.65)+
-  ylim(-0.525, 0.5)+
-  labs(x = paste("PCoA 1 (", all_axis1, "%)", sep = ""), #Annotations for each axis from loadings file
-       y = paste("PCoA 2 (", all_axis2,"%)", sep = ""))
-save_plot(filename = paste0("exploratory/notebook/pcoa_samples_water.png"), pcoa_water, base_height = 5, base_width = 5)
-
-#PCoA data of tissue samples collected from the experiment endpoint with their corresponding stool samples
-pcoa_tissues_v_stool <- all_pcoa_data %>% 
-  filter(sample_type == "water"| sample_type == "cecum"| sample_type == "distal_colon"| sample_type == "proximal_colon"|unique_label %in% stool_corresponding_to_tissues) %>% #select the tissue samples & the corresponding stool samples that were collected from the same mice at the same timepoint
-  ggplot(aes(x=axis1, y=axis2, color = sample_type))+
-  geom_point(size=2)+
-  theme_classic()+
-  theme(legend.position = c(.9, .3))+
-  xlim(-0.425, 0.65)+
-  ylim(-0.525, 0.5)+
-  labs(x = paste("PCoA 1 (", all_axis1, "%)", sep = ""), #Annotations for each axis from loadings file
-       y = paste("PCoA 2 (", all_axis2,"%)", sep = ""))
-save_plot(filename = paste0("exploratory/notebook/pcoa_tissues_v_stools.png"), pcoa_tissues_v_stool, base_height = 5, base_width = 5)
-
 #Read in alpha diversity metrics for all samples----
 diversity_data <- read_tsv("data/process/peg3350.opti_mcc.groups.ave-std.summary") %>%
   filter(method == "ave") %>%
