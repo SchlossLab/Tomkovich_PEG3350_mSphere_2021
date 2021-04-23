e5691e78539390ae2f98b9f5360aee487b577c0a
tomkoset <tomkoset@umich.edu>
2021-04-23 12:45:37 -0400

    Reorder supplemental figures

--- code/figure_S3.R
+++ code/figure_S3.R
@@ -1,9 +1,19 @@
 source("code/utilities.R") #Loads libraries, reads in metadata, functions
 
-a <- ggdraw() + draw_image("results/figures/ml_abund_otu_muribaculum_lineplot.png")
-b <- ggdraw() + draw_image("results/figures/ml_abund_otu_porphyromonadaceae_lineplot.png")
-c <- ggdraw() + draw_image("results/figures/ml_abund_otu_lachnospiraceae_lineplot.png")
+a <- ggdraw() + draw_image("results/figures/5_days_PEG_stool_PCoA_mock.png")
+a_legend <- ggdraw() + draw_image("results/figures/5_days_PEG_pcoa_mock_legend.png")
+a <- plot_grid(a, a_legend, ncol = 1, rel_heights = c(1, .2))
+d <- ggdraw() + draw_image("results/figures/5_days_PEG_tissue_PCoA_mock.png")
+d_legend <- ggdraw() + draw_image("results/figures/5_days_PEG_pcoa_mock_alpha_legend_tissues.png")
+d <- plot_grid(d, d_legend, ncol = 1, rel_heights = c(1, .1))
+top_panel <- plot_grid(a, d, labels = c("A", "D"), label_size = 12, nrow=1)
+b <- ggdraw() + draw_image("results/figures/5_days_PEG_shannon_stools_mock.png")
+e <- ggdraw() + draw_image("results/figures/5_days_PEG_shannon_tissues_mock.png")
+middle_panel <- plot_grid(b, e, labels = c("B", "E"), label_size = 12, ncol=2)
+c <- ggdraw() + draw_image("results/figures/5_days_PEG_genus_lineplot_mock_stools.png")
+f <- ggdraw() + draw_image("results/figures/5_days_PEG_genus_lineplot_mock_tissues.png")
+bottom_panel <- plot_grid(c, f, labels = c("C", "F"), label_size = 12, nrow=1)
 
-plot_grid(a, b, c, labels = c("A", "B", "C"), label_size = 12, ncol=1)+
-  ggsave("results/figures/figure_S3.pdf", width=6.875, height=6.5)+
-  ggsave("submission/figure_S3.pdf", width=6.875, height=6.5)
+plot_grid(top_panel, middle_panel, bottom_panel, labels = NULL, label_size = 12, ncol=1, rel_heights = c(1.4, 1, 1))+
+  ggsave("results/figures/figure_S3.pdf", width=6.875, height=7)+
+  ggsave("submission/figure_S3.pdf", width=6.875, height=7)
---
+++ code/figure_S4.R
@@ -0,0 +1,9 @@
+source("code/utilities.R") #Loads libraries, reads in metadata, functions
+
+a <- ggdraw() + draw_image("results/figures/ml_abund_otu_muribaculum_lineplot.png")
+b <- ggdraw() + draw_image("results/figures/ml_abund_otu_porphyromonadaceae_lineplot.png")
+c <- ggdraw() + draw_image("results/figures/ml_abund_otu_lachnospiraceae_lineplot.png")
+
+plot_grid(a, b, c, labels = c("A", "B", "C"), label_size = 12, ncol=1)+
+  ggsave("results/figures/figure_S4.pdf", width=6.875, height=6.5)+
+  ggsave("submission/figure_S4.pdf", width=6.875, height=6.5)
