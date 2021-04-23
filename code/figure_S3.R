c6e411b2338af9dcdd8cdc14ac3e881b4a34dcd7
tomkoset <tomkoset@umich.edu>
2021-04-23 09:55:45 -0400

    Add lineplots of OTUs from genera of interest

--- code/figure_S3.R
+++ code/figure_S3.R
@@ -1,7 +1,9 @@
 source("code/utilities.R") #Loads libraries, reads in metadata, functions
 
-a <- ggdraw() + draw_image("results/figures/otus_potential_muribaculum_blast_results.png")
+a <- ggdraw() + draw_image("results/figures/ml_abund_otu_muribaculum_lineplot.png")
+b <- ggdraw() + draw_image("results/figures/ml_abund_otu_porphyromonadaceae_lineplot.png")
+c <- ggdraw() + draw_image("results/figures/ml_abund_otu_lachnospiraceae_lineplot.png")
 
-plot_grid(a, labels = c("A"), label_size = 12, ncol=1)+
-  ggsave("results/figures/figure_S3.pdf", width=6.875, height=6)+
-  ggsave("submission/figure_S3.pdf", width=6.875, height=6)
+plot_grid(a, b, c, labels = c("A", "B", "C"), label_size = 12, ncol=1)+
+  ggsave("results/figures/figure_S3.pdf", width=6.875, height=6.5)+
+  ggsave("submission/figure_S3.pdf", width=6.875, height=6.5)
