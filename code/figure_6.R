88f4804868dbc393932a4b42b8a7e524fdaf7dfe
tomkoset <tomkoset@umich.edu>
2021-05-05 10:06:27 -0400

    Add treatment legend to figure

--- code/figure_6.R
+++ code/figure_6.R
@@ -3,10 +3,11 @@ source("code/utilities.R") #Loads libraries, reads in metadata, functions
 #Vert version of Fig 4 16S
 a <- ggdraw() + draw_image("results/figures/post_CDI_PEG_stool_pcoa.png", scale = 1)
 b <- ggdraw() + draw_image("results/figures/post_CDI_PEG_pcoa_legend_vert.png")
+legend <- ggdraw() + draw_image("results/figures/post_CDI_PEG_treatment_legend.png") #Treatment legend for rectangles
 c <- ggdraw() + draw_image("results/figures/post_CDI_PEG_genus_lineplot_fmt.png")
 lineplot <- ggdraw() + draw_image("results/figures/post_CDI_PEG_genus_lineplot_stools.png")
 pcoa1 <- plot_grid(a ,b, labels = c("A", ""), label_size = 12, nrow = 1, rel_heights = c(1,.5), rel_widths = c(1,.5))
 
-plot_grid(pcoa1, c, lineplot, labels = c("", "B", "C"), label_size = 12, nrow = 3)+
-  ggsave("results/figures/figure_6.pdf", width = 6.3, height = 9)+
-  ggsave("submission/figure_6.pdf", width = 6.3, height = 9)
+plot_grid(pcoa1, c, legend, lineplot, labels = c("", "B", "", "C"), label_size = 12, nrow = 4, rel_heights = c(1, 1, .2, 1))+
+  ggsave("results/figures/figure_6.pdf", width = 6, height = 9)+
+  ggsave("submission/figure_6.pdf", width = 6, height = 9)
