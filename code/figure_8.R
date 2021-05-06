e609d7fe1017dc33026c507d3518af77d0281e74
tomkoset <tomkoset@umich.edu>
2021-05-06 13:25:22 -0400

    Remove panel B

--- code/figure_8.R
+++ code/figure_8.R
@@ -3,5 +3,5 @@ source("code/utilities.R") #Loads libraries, reads in metadata, functions
 a <- ggdraw() + draw_image("results/figures/summary_schematic.png")
 
 plot_grid(a, labels = NULL, label_size = 12, ncol=1)+
-  ggsave("results/figures/figure_8.pdf", width=6.875, height=4.5)+
-  ggsave("submission/figure_8.pdf", width=6.875, height=4.5)
+  ggsave("results/figures/figure_8.pdf", width=5, height=4.5)+
+  ggsave("submission/figure_8.pdf", width=5, height=4.5)
